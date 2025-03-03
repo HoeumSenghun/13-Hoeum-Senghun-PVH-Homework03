e1807b75f894e3bde2681c8ff02ce59e6bb4ee55
HoeumSenghun <hoeumsenghun369@gmail.com>
2025-03-03 13:55:09 +0700

    add table

--- src/Main.java
+++ src/Main.java
@@ -15,10 +15,13 @@ public class Main {
     public static void main(String[] args) {
         displayMenu(); // Display the menu table
 
-        // Predefined data
+        // Sample data
         employees.add(new Volunteer(1, "Tina", "PP"));
         employees.add(new HourlySalaryEmployee(3, "Sokha", "BTB", 60, 10.0));
-        employees.add(new SalariedEmployee(7, "Nang", "KP", 900.0, 10.0));
+        employees.add(new Volunteer(4, "Lee", "SR"));
+        employees.add(new HourlySalaryEmployee(6, "Ka", "PV", 50, 10.0));
+        employees.add(new SalariedEmployee(5, "Fy", "KT", 300.0, 20.0));
+        employees.add(new SalariedEmployee(2, "Dana", "KPS", 300.0, 10.0));
 
         while (true) {
             System.out.print("\nChoose an option: ");
@@ -48,13 +51,13 @@ public class Main {
 
     private static void displayMenu() {
         // Create a table with borders
-        Table table = new Table(1, BorderStyle.UNICODE_BOX, ShownBorders.ALL);
-        CellStyle centerStyle = new CellStyle(CellStyle.HorizontalAlign.center);
+        Table table = new Table(1, BorderStyle.UNICODE_BOX, ShownBorders.SURROUND_HEADER_AND_COLUMNS);
 
         // Add header
-        table.addCell("EMS", new CellStyle(CellStyle.HorizontalAlign.center));
+        table.addCell("Employee Management System", new CellStyle(CellStyle.HorizontalAlign.center));
 
         // Add menu items
+        table.setColumnWidth(0, 30, 35);
         table.addCell("1.Insert Employee");
         table.addCell("2.Display Employee");
         table.addCell("3.Update Employee");
@@ -120,17 +123,26 @@ public class Main {
     private static void displayEmployees() {
         // Create a table with borders
         Table table = new Table(9, BorderStyle.UNICODE_BOX, ShownBorders.ALL);
-
+        CellStyle style = new CellStyle(CellStyle.HorizontalAlign.center);
         // Add header
-        table.addCell("Type", new CellStyle(CellStyle.HorizontalAlign.center));
-        table.addCell("ID", new CellStyle(CellStyle.HorizontalAlign.center));
-        table.addCell("Name", new CellStyle(CellStyle.HorizontalAlign.center));
-        table.addCell("Address", new CellStyle(CellStyle.HorizontalAlign.center));
-        table.addCell("Salary", new CellStyle(CellStyle.HorizontalAlign.center));
-        table.addCell("Bonus", new CellStyle(CellStyle.HorizontalAlign.center));
-        table.addCell("Hour", new CellStyle(CellStyle.HorizontalAlign.center));
-        table.addCell("Rate", new CellStyle(CellStyle.HorizontalAlign.center));
-        table.addCell("Pay", new CellStyle(CellStyle.HorizontalAlign.center));
+        table.setColumnWidth(0,25,30);
+        table.setColumnWidth(1,8,15);
+        table.setColumnWidth(2,15,20);
+        table.setColumnWidth(3,10,15);
+        table.setColumnWidth(4,10,15);
+        table.setColumnWidth(5,10,15);
+        table.setColumnWidth(6,8,12);
+        table.setColumnWidth(7,8,12);
+        table.setColumnWidth(8,10,15);
+        table.addCell("Type", style);
+        table.addCell("ID", style);
+        table.addCell("Name", style);
+        table.addCell("Address", style);
+        table.addCell("Salary", style);
+        table.addCell("Bonus", style);
+        table.addCell("Hour", style);
+        table.addCell("Rate", style);
+        table.addCell("Pay", style);
 
         // Add employee rows
         for (StaffMember employee : employees) {
@@ -156,15 +168,15 @@ public class Main {
             }
 
             // Add row to the table
-            table.addCell(type);
-            table.addCell(String.valueOf(employee.id));
-            table.addCell(employee.name);
-            table.addCell(employee.address);
-            table.addCell(salary);
-            table.addCell(bonus);
-            table.addCell(hour);
-            table.addCell(rate);
-            table.addCell(String.format("$%.2f", pay));
+            table.addCell(type, style);
+            table.addCell(String.valueOf(employee.id), style);
+            table.addCell(employee.name, style);
+            table.addCell(employee.address, style);
+            table.addCell(salary, style);
+            table.addCell(bonus, style);
+            table.addCell(hour, style);
+            table.addCell(rate, style);
+            table.addCell(String.format("$%.2f", pay), style);
         }
 
         // Render the table
